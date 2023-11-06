4200bb8045eb974eafe4242359ecfa8fff151b88
A <bitranjit123@gmail.com>
2023-11-06 20:19:46 +0800

    Removed

--- PaySlip_DelRosario.java
+++ PaySlip_DelRosario.java
@@ -85,7 +85,7 @@ public class PaySlip_DelRosario {
     return pagIbigDeduction;
   }
 
-  public double getPhilheatlDeduction() {I got incredibly confused by our ac
+  public double getPhilheatlDeduction() {
     philHealthDeduction = basicSalary * 0.35;
     if (philHealthDeduction > 2450) {
       philHealthDeduction = 2450;
